232a41615398cbc42d02c7a7dcda97ae26c57db1
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-05-04 21:03:22 +0200

    fix clean input after search

--- js/main.js
+++ js/main.js
@@ -2,12 +2,9 @@ $(document).ready(function (){
 
   //handlebars
   var source = $("#handlebars-template").html();
-  //console.log(source);
   var template = Handlebars.compile(source);
   
-
   //referenze
-
   var inputSearch = $("#search");
   var btnSearch = $("#search-btn");
   
@@ -32,39 +29,59 @@ $(document).ready(function (){
 
 
 function apiCercaFilm (inputSearch,template){
+  console.log("chiamata funzione");
   
-  var lista = $(".films");
-  var query = inputSearch.val().toLowerCase().trim();
+  if(inputSearch.val() != "" ){
 
-  $.ajax({
-    url: "https://api.themoviedb.org/3/search/movie",
-    method: "GET",
-    data: {
-      api_key: "676860408bcc47c8ae82c5de9ed14e0e",
-      language: "it-IT",
-      query: query
-    },
-    success: function(res){
-      lista.html("");
-      
-      var datiFilm = res.results;
-      
-      for(var i = 0; i < datiFilm.length; i++){
+    var lista = $(".films");
+    var query = inputSearch.val().toLowerCase().trim();
+    console.log("chiamata api");
+    
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/movie",
+      method: "GET",
+      data:{
+        api_key: "676860408bcc47c8ae82c5de9ed14e0e",
+        language: "it-IT",
+        query: query
+      },
+      success: function(res){
+        lista.html("");
+
+        //sperimento foreach
+        res.results.forEach(function(datiFilm){
+          var film ={
+            title: datiFilm.title,
+            originalTitle: datiFilm.original_title,
+            originalLanguage: datiFilm.original_language,
+            voteAverage: datiFilm.vote_average
+          }
+          
+          lista.append(template(film));
+          inputSearch.val("");
+        })
         
-        var film ={
-          title: datiFilm[i].title,
-          originalTitle: datiFilm[i].original_title,
-          originalLanguage: datiFilm[i].original_language,
-          voteAverage: datiFilm[i].vote_average
-        }
-
-        lista.append( template(film));
-        inputSearch.empty("")
+        //var datiFilm = res.results;
+  
+        /* for(var i = 0; i < datiFilm.length; i++){
+          
+          var film ={
+            title: datiFilm[i].title,
+            originalTitle: datiFilm[i].original_title,
+            originalLanguage: datiFilm[i].original_language,
+            voteAverage: datiFilm[i].vote_average
+          }
+  
+          lista.append( template(film));
+          inputSearch.empty()
+        } */
+      },
+      error: function(){
+        console.log("Errore Api");
       }
-    },
-    error: function(){
-      console.log("Errore Api");
-    }
-  });
 
-}
+    });//fine chiamata ajax
+
+  }//fine if confronto
+
+}//fine apiCercaFilm
