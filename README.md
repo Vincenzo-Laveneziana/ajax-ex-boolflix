91a044323d264893ef9fe61918276be6293f1e8a
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-05-05 12:50:54 +0200

    add TV series call, changed vote rounding

--- README.md
+++ README.md
@@ -1,18 +1,8 @@
 # ajax-ex-boolflix **:open_file_folder:**
 
-:man_student:--Task: **Milestone 1**
+:man_student:--Task: 
 
-Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: 
-
-- Titolo
-
-- Titolo Originale
-
-- Lingua Originale
-
-- Voto (media)
-
-  Utilizzare un template Handlebars per mostrare ogni singolo film trovato.
+[Task.pdf]: 
 
  ​ ​ :desktop_computer: Project Preview:
 
--- js/main.js
+++ js/main.js
@@ -10,13 +10,19 @@ $(document).ready(function (){
   
   
   btnSearch.click(function(){
+   
+    apiCercaSerieTv(inputSearch,template);
     apiCercaFilm(inputSearch,template); 
+    
   })
 
   inputSearch.keyup(function(event){
 
     if(event.which === 13){
+      
+      apiCercaSerieTv(inputSearch,template);
       apiCercaFilm(inputSearch,template);
+      
     }
   })
   
@@ -27,15 +33,67 @@ $(document).ready(function (){
   FUNZIONI
 */
 
-
-function apiCercaFilm (inputSearch,template){
-  console.log("chiamata funzione");
+//cerca serie tv
+function apiCercaSerieTv(inputSearch,template){
+  console.log("chiamata serie tv");
   
   if(inputSearch.val() != "" ){
 
     var lista = $(".films");
     var query = inputSearch.val().toLowerCase().trim();
-    console.log("chiamata api");
+    //console.log("chiamata api");
+    
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/tv",
+      method: "GET",
+      data:{
+        api_key: "676860408bcc47c8ae82c5de9ed14e0e",
+        language: "it-IT",
+        query: query
+      },
+      success: function(res){
+        lista.html("");
+        
+
+        if(res.results.length > 0){
+
+            //sperimento foreach
+          res.results.forEach(function(datiFilm){
+            var film ={
+              title: datiFilm.name,
+              originalTitle: datiFilm.original_name,
+              originalLanguage: datiFilm.original_language,
+              voteAverage: votoStelle(datiFilm.vote_average),
+              tipoSerie : "serie-tv"
+            }
+            
+            lista.append(template(film));
+            //inputSearch.val("");
+          })
+
+        }else{
+          alert("nessuna serie tv trovata")
+        }
+        
+      },
+      error: function(){
+        console.log("Errore Api");
+      }
+    });//fine chiamata ajax
+  }//fine if confronto
+}//fine apiCercaSerieTv
+
+
+
+//cerca film
+function apiCercaFilm (inputSearch,template){
+  console.log("chiamata film");
+  
+  if(inputSearch.val() != "" ){
+
+    var lista = $(".films");
+    var query = inputSearch.val().toLowerCase().trim();
+    //console.log("chiamata api");
     
     $.ajax({
       url: "https://api.themoviedb.org/3/search/movie",
@@ -46,42 +104,42 @@ function apiCercaFilm (inputSearch,template){
         query: query
       },
       success: function(res){
-        lista.html("");
-
-        //sperimento foreach
-        res.results.forEach(function(datiFilm){
-          var film ={
-            title: datiFilm.title,
-            originalTitle: datiFilm.original_title,
-            originalLanguage: datiFilm.original_language,
-            voteAverage: datiFilm.vote_average
-          }
-          
-          lista.append(template(film));
-          inputSearch.val("");
-        })
+        //lista.html("");
         
-        //var datiFilm = res.results;
-  
-        /* for(var i = 0; i < datiFilm.length; i++){
-          
-          var film ={
-            title: datiFilm[i].title,
-            originalTitle: datiFilm[i].original_title,
-            originalLanguage: datiFilm[i].original_language,
-            voteAverage: datiFilm[i].vote_average
-          }
-  
-          lista.append( template(film));
-          inputSearch.empty()
-        } */
+
+        if(res.results.length > 0){
+
+            //sperimento foreach
+          res.results.forEach(function(datiFilm){
+            var film ={
+              title: datiFilm.title,
+              originalTitle: datiFilm.original_title,
+              originalLanguage: datiFilm.original_language,
+              voteAverage: votoStelle(datiFilm.vote_average),
+              tipoSerie : "film"
+            }
+            
+            lista.append(template(film));
+            //inputSearch.val("");
+          })
+
+        }else{
+          alert("nessun film trovato")
+        }
+        
       },
       error: function(){
         console.log("Errore Api");
       }
-
     });//fine chiamata ajax
-
   }//fine if confronto
+}//fine apiCercaFilm 
 
-}//fine apiCercaFilm
+
+function votoStelle(voto){
+  
+  //arrotonamento per eccesso
+  var voto = Math.round(voto*0.5);
+
+  return voto;
+}
