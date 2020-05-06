985fbf4849d0f8a6490b08aed0aa0f3f48f529d8
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-05-06 16:58:33 +0200

    end millestone 3-4. Graphic care start

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,9 @@ $(document).ready(function (){
   var lista = $(".films");
   var resultTV = $(".tv-series");
   var resultFilm = $(".film-series");
-  
+  var coverFilm =  $(".cover-film");
+  var coverFilmDetails = $(".cover-film-details");
+
   //al click
   btnSearch.click(function(){
     chiamaFunzioni(lista,resultTV,resultFilm,inputSearch,template)
@@ -26,7 +28,7 @@ $(document).ready(function (){
       chiamaFunzioni(lista,resultTV,resultFilm,inputSearch,template)
     }
   })
-  
+
 });//fine ready
 
 
@@ -39,8 +41,6 @@ function chiamaFunzioni(lista,resultTV,resultFilm,inputSearch,template){
  
   if(inputSearch.val() !== "" ){
     pulisciRisultati(lista,resultTV,resultFilm);
-    /* apiCercaSerieTv(inputSearch,template,lista);
-    apiCercaFilm(inputSearch,template,lista);  */   
     search(inputSearch,template,lista);
   }else{
     alert("Nessun carattere inserito nella ricerca")
@@ -49,78 +49,10 @@ function chiamaFunzioni(lista,resultTV,resultFilm,inputSearch,template){
 }
 
 
-/* 
-QUESTE 2 FUNZIONI MOMENTANEAMENTE LE LASCIO COMMENTATE
-*/
-
-/* //cerca serie tv
-function apiCercaSerieTv(inputSearch,template,lista){
- 
-  console.log("chiamata serie tv");
-  var query = inputSearch.val().toLowerCase().trim();
-  
-  $.ajax({
-    url: "https://api.themoviedb.org/3/search/tv",
-    method: "GET",
-    data:{
-      api_key: "676860408bcc47c8ae82c5de9ed14e0e",
-      language: "it-IT",
-      query: query
-    },
-    success: function(res){
-      var movie = res.results;
-
-      if(movie.length > 0){
-
-        print(movie, template, lista,"Serie-tv" );
-      }else{
-        $(".tv-series").text("Nessuna serie tv trovata")
-      }
-    },
-    error: function(){
-      console.log("Errore Api");
-    }
-  });//fine chiamata ajax
-}//fine apiCercaSerieTv
-
-
-//cerca film
-function apiCercaFilm (inputSearch,template,lista){
-  
-  console.log("chiamata film");
- 
-  
-  var query = inputSearch.val().toLowerCase().trim();
-  
-  $.ajax({
-    url: "https://api.themoviedb.org/3/search/movie",
-    method: "GET",
-    data:{
-      api_key: "676860408bcc47c8ae82c5de9ed14e0e",
-      language: "it-IT",
-      query: query
-    },
-    success: function(res){
-      var movie = res.results;
-
-      if(movie.length > 0){
-
-        print(movie, template, lista, "Film");        
-      }else{
-        $(".film-series").text("Nessun film trovato")
-      }
-
-    },
-    error: function(){
-      console.log("Errore Api");
-    }
-  });//fine chiamata ajax
-}//fine apiCercaFilm  */
-
-
-
-
+//funzione ricerca dei film/serie-tv
 function search(inputSearch,template,lista){
+  console.log("chiata api");
+  
   var query = inputSearch.val().toLowerCase().trim();
 
   var urlApi = ["movie", "tv"];
@@ -144,9 +76,7 @@ function search(inputSearch,template,lista){
           } else if(urlApi == "tv"){
             print(movie, template, lista,"Serie-tv" );    
           }
-  
-          
-           
+
         }else{
 
           if(urlApi == "movie"){
@@ -164,9 +94,7 @@ function search(inputSearch,template,lista){
     });//fine chiamata ajax
   })
 
-  
-  
-}
+}//fine search
 
 
 function print(movie, template, lista, type){
@@ -174,8 +102,9 @@ function print(movie, template, lista, type){
   movie.forEach(function(dati){
 
     var title, originalTitle;
+    var overviewP = dati.overview;
+    //overviewP = overviewP.substr(1, 200);
 
-    
     if(type == "Film"){
       title = dati.title;
       originalTitle = dati.original_title;
@@ -185,16 +114,28 @@ function print(movie, template, lista, type){
     }
 
     var context ={
+      posterPath: coverImg(dati.poster_path),
       title: title,
       originalTitle: originalTitle,
       originalLanguage: flagLang(dati.original_language),
       voteAverage: votoStelle(dati.vote_average),
-      tipoSerie : type
+      tipoSerie : type,
+      overview : overviewP 
     }
     
     lista.append(template(context));
   })
     
+}//fine print
+
+// inserimento img cover
+function coverImg(poster){
+
+  if (poster != null){
+    return poster = "https://image.tmdb.org/t/p/w342/" + poster;
+  } else{
+    return poster = "assets/img/no-poster.png";
+  }
 }
 
 //conversione rating in star
